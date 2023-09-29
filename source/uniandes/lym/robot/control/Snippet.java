daca22953cbbb2bae166d2a79dee7e9a53b1cb5f
Santiago Castro <s.castrod@uniandes.edu.co>
2023-09-28 22:38:43 -0500

    first commit

---
+++ source/uniandes/lym/robot/control/Snippet.java
@@ -0,0 +1,25 @@
+package uniandes.lym.robot.control;
+
+public class Snippet {
+	/*
+	Gramatica para el Robot:
+	Autor: Alejandro Arango
+	Modificado por: Silvia Takahashi
+	Modificado por: Juan Pablo Morales.
+	Agregadas producciones para manejar globos
+	Modificado por Silvia Takahashi
+	
+	
+	 **/
+	
+	options{
+	
+	LOOKAHEAD=1;IGNORE_CASE=true;STATIC=false;
+	
+	}
+	
+	PARSER_BEGIN(Robot)
+	
+	
+}
+
